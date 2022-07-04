5b06b137cf66065d72ce64bd92b4053e96e2a00c
Youngho <kyh951019@gmail.com>
2022-07-04 15:10:34 +0900

    Add Testcase for configuration struct with .env file.

--- interact/tests/suite1.rs
+++ interact/tests/suite1.rs
@@ -13,14 +13,19 @@ impl Config {
     pub fn read_from_env() -> Self {
         serde_json::from_str(
             &std::fs::read_to_string(
-                std::env::vars()
-                    .nth(1)
-                    .expect("Environment variable for the config file path is missing"),
+                std::env::var("ENVIRONMENT")
+                .expect("Environment variable for the config file path is missing"),
             )
             .expect("Failed to locate the config file"),
         )
         .expect("Failed to parse the config")
     }
+}
+
+#[test]
+fn check_configuration() {
+    let _config: Config = Config::read_from_env();
+    assert_eq!(_config.full_node_url.is_empty(), false)
 }
 
 #[tokio::test]
