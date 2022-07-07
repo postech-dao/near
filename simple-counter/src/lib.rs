6fd8c6028e966495a9e7be3b75e0fb17aa9de7f9
EunSung Cha <ender3553@postech.ac.kr>
2022-07-07 14:46:43 +0900

    modify simple-counter

--- simple-counter/src/lib.rs
+++ simple-counter/src/lib.rs
@@ -4,47 +4,39 @@ use near_sdk::{env, near_bindgen, AccountId};
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct State {
-    count: u64, // TODO,
+    count: u64,
 }
 
 #[near_bindgen]
 impl State {
     #[init]
     pub fn new(count: u64) -> Self {
-        //TODO
         State { count }
     }
 
     pub fn get_num(&self) -> u64 {
-        //TODO
         self.count
     }
 
     pub fn increment(&mut self) {
-        //TODO
-        self.count +=1;
+        self.count += 1;
     }
 
     pub fn decrement(&mut self) {
-        //TODO
-        self.count -=1;
+        self.count -= 1;
     }
 
     pub fn reset(&mut self) {
-        //TODO
-        self.count =0;
+        self.count = 0;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::u64::MIN;
-    use std::u64::MAX;
     use super::State;
 
     #[test]
     fn increment() {
-        // TODO
         let mut contract = State::new(0);
         contract.increment();
         assert_eq!(1, contract.get_num());
@@ -52,7 +44,6 @@ mod tests {
 
     #[test]
     fn decrement() {
-        // TODO
         let mut contract = State::new(1);
         contract.decrement();
         assert_eq!(0, contract.get_num());
@@ -60,7 +51,6 @@ mod tests {
 
     #[test]
     fn reset() {
-        // TODO
         let mut contract = State::new(5);
         contract.reset();
         assert_eq!(0, contract.get_num());
@@ -69,18 +59,14 @@ mod tests {
     #[test]
     #[should_panic]
     fn panics_on_overflow() {
-        // TODO
-        let mut contract = State::new(MAX);
+        let mut contract = State::new(u64::MAX);
         contract.increment();
     }
 
     #[test]
     #[should_panic]
     fn panic_on_underflow() {
-        // TODO
-        let mut contract = State::new(MIN);
+        let mut contract = State::new(u64::MIN);
         contract.decrement();
     }
 }
-
-
