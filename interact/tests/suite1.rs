59b3a9150ac25438376908793e252454fb1555a2
unajun <bjyoon513@gmail.com>
2022-07-09 23:21:08 +0900

    feat(interact): implement simple test node interaction except contract interaction

--- interact/tests/suite1.rs
+++ interact/tests/suite1.rs
@@ -1,8 +1,12 @@
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::{thread, time};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    full_node_url: String,
+    network_id: String,
+    test_node_url: String,
     account_public: String,
     account_private: String,
     wasm_binary_path: String,
@@ -23,28 +27,94 @@ impl Config {
 }
 
 #[tokio::test]
-#[ignore]
 async fn check_connection() {
     let _config = Config::read_from_env();
     // check whether the full node is responding by a simple request
-    unimplemented!();
+    let client = Client::new();
+    let payload = json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "status",
+            "params": []
+    });
+
+    let res = client
+        .post(&_config.test_node_url)
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    let json = res.json::<Value>().await.unwrap();
+    assert_eq!(json["result"]["chain_id"], _config.network_id);
 }
 
 #[tokio::test]
-#[ignore]
 async fn check_block_number() {
     let _config = Config::read_from_env();
     // check the latest block number recognized by the full node **twice** with some delay,
     // so that we can assure that the full node is properly updating its blocks
-    unimplemented!();
+    let client = Client::new();
+    let payload = json!({
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "block",
+        "params": {
+          "finality": "final"
+        }
+    });
+
+    let first_res = client
+        .post(&_config.test_node_url)
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    let first_json = first_res.json::<Value>().await.unwrap();
+
+    thread::sleep(time::Duration::from_secs(2));
+
+    let second_res = client
+        .post(&_config.test_node_url)
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    let second_json = second_res.json::<Value>().await.unwrap();
+
+    assert!(
+        second_json["result"]["header"]["height"].as_u64()
+            > first_json["result"]["header"]["height"].as_u64()
+    );
 }
 
 #[tokio::test]
-#[ignore]
 async fn check_account() {
     let _config = Config::read_from_env();
     // by requesting the full node, check whether the account given by the config has enough native token to pay gas fee
-    unimplemented!();
+    let client = Client::new();
+    let payload = json!({
+        "jsonrpc": "2.0",
+        "id": "dontcare",
+        "method": "query",
+        "params": {
+        "request_type": "view_account",
+        "finality": "final",
+        "account_id": "nearkat.testnet"
+        }
+    });
+
+    let res = client
+        .post(&_config.test_node_url)
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    let json = res.json::<Value>().await.unwrap();
+    assert_eq!(json["error"], Value::Null);
 }
 
 #[tokio::test]
