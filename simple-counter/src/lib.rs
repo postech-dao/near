cbb9d4190f3561c813a480de087cb4f814b82f8d
junsoo37 <junsoor37@gmail.com>
2022-07-10 09:17:08 +0900

    remove unused package

--- simple-counter/src/lib.rs
+++ simple-counter/src/lib.rs
@@ -1,29 +1,82 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId};
+use near_sdk::collections::UnorderedSet;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{near_bindgen, AccountId, BorshStorageKey};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct State {
     count: u64,
+    auth_ids: UnorderedSet<AccountId>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Transaction {
+    from: AccountId,
+    value: u64,
+}
+
+#[derive(BorshStorageKey, BorshSerialize)]
+pub enum StorageKey {
+    AuthAccount,
 }
 
 #[near_bindgen]
 impl State {
     #[init]
-    pub fn new(count: u64) -> Self {
-        State { count: count }
+    pub fn new(val: u64, auths: Vec<AccountId>) -> Self {
+        let mut this = Self {
+            count: val,
+            auth_ids: UnorderedSet::new(StorageKey::AuthAccount),
+        };
+        for auth_id in auths {
+            this.auth_ids.insert(&auth_id);
+        }
+
+        this
+    }
+
+    pub fn validate_transaction(&self, trans: &Transaction) -> bool {
+        self.authenticate_id(&trans.from) && State::check_transaction_value(trans.value)
+    }
+
+    pub fn authenticate_id(&self, from_account: &AccountId) -> bool {
+        if self.auth_ids.contains(from_account) {
+            true
+        } else {
+            panic!(
+                "Validation failed: account {} is not in auth_ids",
+                from_account
+            );
+        }
+    }
+
+    pub fn check_transaction_value(value: u64) -> bool {
+        let max_transaction_value = 10;
+        if value <= max_transaction_value {
+            true
+        } else {
+            panic!(
+                "Validation failed: transaction value {} larger than max_transaction value {}",
+                value, max_transaction_value
+            )
+        }
     }
 
     pub fn get_num(&self) -> u64 {
         self.count
     }
 
-    pub fn increment(&mut self) {
-        self.count += 1;
+    pub fn increment(&mut self, trans: &Transaction) {
+        if self.validate_transaction(trans) {
+            self.count += trans.value;
+        }
     }
 
-    pub fn decrement(&mut self) {
-        self.count -= 1;
+    pub fn decrement(&mut self, trans: &Transaction) {
+        if self.validate_transaction(trans) {
+            self.count -= trans.value;
+        }
     }
 
     pub fn reset(&mut self) {
@@ -33,44 +86,83 @@ impl State {
 
 #[cfg(test)]
 mod tests {
-    use crate::State;
+    use super::*;
+    use crate::Transaction;
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
 
     #[test]
     fn increment() {
-        let mut contract = State { count: 0 };
-        contract.increment();
-        assert_eq!(1, contract.get_num());
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+
+        let auths = vec![accounts(0)];
+        let mut contract = State::new(10, auths);
+        let transaction = Transaction {
+            from: accounts(0),
+            value: 5,
+        };
+
+        contract.increment(&transaction);
+        assert_eq!(15, contract.get_num());
     }
 
     #[test]
     fn decrement() {
-        let mut contract = State { count: 1 };
-        contract.decrement();
-        assert_eq!(0, contract.get_num());
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+        let auths = vec![accounts(0)];
+        let mut contract = State::new(10, auths);
+        let transaction = Transaction {
+            from: accounts(0),
+            value: 5,
+        };
+
+        contract.decrement(&transaction);
+        assert_eq!(5, contract.get_num());
     }
 
     #[test]
     fn reset() {
-        let mut contract = State { count: 1 };
+        let auths = vec![accounts(0)];
+        let mut contract = State::new(10, auths);
         contract.reset();
         assert_eq!(0, contract.get_num());
     }
 
     #[test]
     #[should_panic]
-    fn panics_on_overflow() {
-        let mut contract = State {
-            count: u64::max_value(),
+    fn test_validate_auth() {
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+        let auths = vec![accounts(0)];
+        let mut contract = State::new(10, auths);
+        let transaction = Transaction {
+            from: accounts(1),
+            value: 5,
         };
-        contract.increment()
+        contract.increment(&transaction);
     }
 
     #[test]
     #[should_panic]
-    fn panic_on_underflow() {
-        let mut contract = State {
-            count: u64::min_value(),
+    fn test_validate_max_value() {
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+        let auths = vec![accounts(0)];
+        let mut contract = State::new(10, auths);
+        let transaction = Transaction {
+            from: accounts(0),
+            value: 100,
         };
-        contract.decrement()
+        contract.increment(&transaction);
     }
 }
