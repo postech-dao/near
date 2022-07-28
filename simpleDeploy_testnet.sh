6df4ab8d559e70592ca248f8c24c3d7bdcc0367f
Basten1209 <seungjunoh@postech.ac.kr>
2022-07-28 15:13:29 +0900

    Add SimpleDeploy script in testnet&mainnet

---
+++ simpleDeploy_testnet.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "1. Build"
+
+yarn Build
+
+echo
+echo "2. Deploy on testnet"
+
+near dev-deploy ./build/debug/simple-counter.wasm
+
+echo
+echo "3. dev-deploy done."
+echo "account ID : dev-123-456 "
+
+echo "near call \$CONTRACT init --accountId \$CONTRACT"
+echo
+
+exit 0
