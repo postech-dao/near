d9bdcb89ca5fee5abda2d351602f9a026a83078c
Youngho <kyh951019@gmail.com>
2022-09-01 17:35:26 +0900

    Add: implement basic light client

--- light-client/src/lib.rs
+++ light-client/src/lib.rs
@@ -1,31 +1,22 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
-use pdao_colony_contract_common::LightClientUpdateMessage;
-use std::time::{SystemTime, UNIX_EPOCH};
-
-/// TODO: replace this with the proper type.
-pub type Header = String;
-/// TODO: replace this with the proper type.
-pub type BlockFinalizationProof = String;
-/// TODO: replace this with the proper type.
-pub type MerkleProof = String;
-/// TODO: replace this with the proper type.
-pub type Timestamp = u128;
+use near_sdk::collections::Vector;
+use near_sdk::{near_bindgen, BorshStorageKey};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct LightClient {
-    pub height: u64,
-    pub last_header: Header,
-    pub timestamp: Timestamp,
+    pub light_client_vector: Vector<pdao_colony_contract_common::LightClient>,
+}
+
+#[derive(BorshStorageKey, BorshSerialize)]
+pub enum StorageKey {
+    LightClient,
 }
 
 impl Default for LightClient {
     fn default() -> Self {
         Self {
-            height: 0,
-            last_header: "".to_string(),
-            timestamp: 0,
+            light_client_vector: Vector::new(StorageKey::LightClient),
         }
     }
 }
@@ -33,64 +24,57 @@ impl Default for LightClient {
 #[near_bindgen]
 impl LightClient {
     #[init]
-    pub fn new(initial_header: Header) -> Self {
-        let time = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_millis();
+    pub fn new() -> Self {
         Self {
-            height: 0,
-            last_header: initial_header,
-            timestamp: time,
+            light_client_vector: Vector::new(StorageKey::LightClient),
         }
     }
 
-    pub fn update(&mut self, message: LightClientUpdateMessage) -> bool {
-        if message.proof.as_str() == "valid" {
-            self.height += 1;
-            self.last_header = message.header;
-            true
+    pub fn update_light_client(
+        &mut self,
+        message: pdao_beacon_chain_common::message::DeliverableMessage,
+    ) {
+        let light_client = pdao_colony_contract_common::LightClient::new(
+            "initial_header".to_string(),
+            "near".to_string(),
+        );
+        self.light_client_vector.push(&light_client);
+    }
+
+    pub fn get_light_client_header(&self) -> pdao_colony_contract_common::Header {
+        if let Some(lastest_light_client) = self
+            .light_client_vector
+            .get(self.light_client_vector.len() - 1)
+        {
+            lastest_light_client.last_header
         } else {
-            false
+            "initial_header".to_string()
         }
     }
 
     pub fn verify_commitment(
         &self,
-        _message: Vec<u8>,
-        block_height: u64,
-        proof: MerkleProof,
+        message: pdao_beacon_chain_common::message::DeliverableMessage,
     ) -> bool {
-        proof.as_str() == "valid" && block_height == self.height
+        let merkle_proof = "merkle_proof".to_string();
+        let mut result = false;
+        for i in 0..self.light_client_vector.len() {
+            if let Some(light_client) = self.light_client_vector.get(i) {
+                result = light_client.verify_commitment(message.clone(), i, merkle_proof.clone());
+                if result {
+                    break;
+                }
+            }
+        }
+        result
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::LightClient;
-    use pdao_colony_contract_common::LightClientUpdateMessage;
 
     #[test]
-    fn update() {
-        let mut light_client = LightClient::new(String::from("0x1"));
-        let light_client_update_messeage = LightClientUpdateMessage {
-            header: String::from("0x2"),
-            proof: String::from("valid"),
-        };
-        light_client.update(light_client_update_messeage);
-        assert_eq!(light_client.last_header, String::from("0x2"));
-    }
-
-    #[test]
-    fn verify_commitment() {
-        let mut light_client: LightClient = LightClient::new(String::from("0x1"));
-        let light_client_update_messeage = LightClientUpdateMessage {
-            header: String::from("0x2"),
-            proof: String::from("valid"),
-        };
-        light_client.update(light_client_update_messeage);
-
-        let status = light_client.verify_commitment(vec![], 1, String::from("valid"));
-        assert_eq!(status, true);
+    fn transaction() {
+        assert!(true);
     }
 }
