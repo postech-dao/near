f700bbcd0977237460f411f6420016aa94717635
Youngho <kyh951019@gmail.com>
2022-09-04 17:45:52 +0900

    feat: near light client

--- light-client/src/lib.rs
+++ light-client/src/lib.rs
@@ -1,11 +1,10 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::Vector;
 use near_sdk::{near_bindgen, BorshStorageKey};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct LightClient {
-    pub light_client_vector: Vector<pdao_colony_contract_common::LightClient>,
+    pub light_client: pdao_colony_contract_common::LightClient,
 }
 
 #[derive(BorshStorageKey, BorshSerialize)]
@@ -16,7 +15,11 @@ pub enum StorageKey {
 impl Default for LightClient {
     fn default() -> Self {
         Self {
-            light_client_vector: Vector::new(StorageKey::LightClient),
+            light_client: pdao_colony_contract_common::LightClient {
+                height: 0,
+                last_header: "last_header".to_string(),
+                chain_name: "near".to_string(),
+            },
         }
     }
 }
@@ -26,55 +29,74 @@ impl LightClient {
     #[init]
     pub fn new() -> Self {
         Self {
-            light_client_vector: Vector::new(StorageKey::LightClient),
+            light_client: pdao_colony_contract_common::LightClient {
+                height: 0,
+                last_header: "last_header".to_string(),
+                chain_name: "near".to_string(),
+            },
         }
     }
 
-    pub fn update_light_client(
-        &mut self,
-        message: pdao_beacon_chain_common::message::DeliverableMessage,
-    ) {
-        let light_client = pdao_colony_contract_common::LightClient::new(
-            "initial_header".to_string(),
-            "near".to_string(),
-        );
-        self.light_client_vector.push(&light_client);
+    pub fn update(&mut self, header: pdao_colony_contract_common::Header) {
+        self.light_client.height += 1;
+        self.light_client.last_header = header;
     }
 
-    pub fn get_light_client_header(&self) -> pdao_colony_contract_common::Header {
-        if let Some(lastest_light_client) = self
-            .light_client_vector
-            .get(self.light_client_vector.len() - 1)
-        {
-            lastest_light_client.last_header
-        } else {
-            "initial_header".to_string()
-        }
+    pub fn get_header(&self) -> pdao_colony_contract_common::Header {
+        self.light_client.last_header.clone()
     }
 
     pub fn verify_commitment(
         &self,
         message: pdao_beacon_chain_common::message::DeliverableMessage,
+        block_height: u64,
+        proof: pdao_colony_contract_common::MerkleProof,
     ) -> bool {
-        let merkle_proof = "merkle_proof".to_string();
-        let mut result = false;
-        for i in 0..self.light_client_vector.len() {
-            if let Some(light_client) = self.light_client_vector.get(i) {
-                result = light_client.verify_commitment(message.clone(), i, merkle_proof.clone());
-                if result {
-                    break;
-                }
-            }
-        }
-        result
+        self.light_client
+            .verify_commitment(message, block_height, proof)
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use crate::LightClient;
+
     #[test]
-    fn transaction() {
-        assert!(true);
+    fn test_update() {
+        let header = "new_last_header".to_string();
+        let mut light_client_contract = LightClient::new();
+        light_client_contract.update(header);
+        assert_eq!(
+            light_client_contract.light_client.last_header,
+            "new_last_header".to_string()
+        )
+    }
+
+    #[test]
+    fn test_get_header() {
+        let light_client_contract = LightClient::new();
+        assert_eq!(
+            light_client_contract.get_header(),
+            "last_header".to_string()
+        )
+    }
+
+    #[test]
+    fn test_verify_commitment() {
+        let light_client_contract = LightClient::new();
+        let message =
+            pdao_beacon_chain_common::message::DeliverableMessage::NonFungibleTokenTransfer(
+                pdao_beacon_chain_common::message::NonFungibleTokenTransfer {
+                    collection_address: "collection_address".to_string(),
+                    token_index: "token_index".to_string(),
+                    receiver_address: "receiver_address".to_string(),
+                    contract_sequence: 0,
+                },
+            );
+        let block_height = 0;
+        let proof = "valid".to_string();
+        let verify_result = light_client_contract.verify_commitment(message, block_height, proof);
+        assert!(verify_result)
     }
 }
